72e8894f419c073c77df699e0943fa92e4760f52
Shammas Mazhar <shammasmazhar50@users.noreply.github.com>
2026-09-15 23:08:53 +0500

    Fix runaway requestAnimationFrame loop in SmoothScroll

--- src/components/SmoothScroll.jsx
+++ src/components/SmoothScroll.jsx
@@ -18,14 +18,17 @@ const SmoothScroll = ({ children }) => {
 
     lenisRef.current = lenis;
 
+    let rafId;
+
     function raf(time) {
       lenis.raf(time);
-      requestAnimationFrame(raf);
+      rafId = requestAnimationFrame(raf);
     }
 
-    requestAnimationFrame(raf);
+    rafId = requestAnimationFrame(raf);
 
     return () => {
+      cancelAnimationFrame(rafId);
       lenis.destroy();
     };
   }, []);
